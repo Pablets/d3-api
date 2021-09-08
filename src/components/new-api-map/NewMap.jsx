38809de9b6edc0497e85dc57a35b25a1f0b7aaa0
Pablets <godoypablom@gmail.com>
2021-09-08 07:21:43 -0300

    Added opacity animatiion, merge testing

--- src/components/new-api-map/NewMap.jsx
+++ src/components/new-api-map/NewMap.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {mapApi} from '../../lib/new.map.api';
+import {feature, merge} from 'topojson-client';
 // import useFetchData from '../../hooks/useFetchData';
 
 const url = 'https://elecciones2017.lanacion.com.ar/multimedia/proyectos/17/elecciones/elecciones_2017_paso_mapa_resultados/data/arg_opt.json';
 
 const NewMap = () => {
+	const gba = ['02003', '02128', '02035', '02053', '02089', '02134', '02080', '02130', '02129', '02052', '02079', '02133', '02061', '02118', '02077', '02062', '02070', '02036', '02132', '02038', '02092', '02122', '02011', '02004', '02106', '02113', '02105'];
+
 	const [provinceName, setProvinceName] = React.useState('Buenos Aires');
 	const [svgMap, setSvgMap] = React.useState(null);
 
@@ -21,7 +24,17 @@ const NewMap = () => {
 				return;
 			}
 			response.json().then(fetchedData => {
-				// console.log('linea24:', worlddata);
+				// console.log('linea24:', fetchedData.objects.provs);
+
+				let {geometries} = fetchedData.objects.dptos;
+
+				const newProvs = geometries.filter(el => el.id.startsWith('02'));
+				const interiorBA = newProvs.filter(el => !gba.includes(el.id));
+				const granBA = newProvs.filter(el => gba.includes(el.id));
+				console.log(granBA);
+				fetchedData.objects.provs.geometries.push(merge(granBA));
+				console.log(fetchedData.objects.provs.geometries);
+
 				setSvgMap(mapApi(nodeRef, fetchedData, cb));
 			});
 		});
--- src/lib/new.map.api.js
+++ src/lib/new.map.api.js
@@ -1,7 +1,8 @@
 import * as d3 from 'd3';
-import {feature, mesh} from 'topojson-client';
+import { feature, mesh } from 'topojson-client';
 
 export function mapApi(nodeRef, ar, cb) {
+	let active = d3.select(null)
 	const width = nodeRef.current.clientWidth;
 	const height = nodeRef.current.clientHeight;
 	const container = d3.select(nodeRef.current);
@@ -10,7 +11,7 @@ export function mapApi(nodeRef, ar, cb) {
 
 	const zoom = d3
 		.zoom()
-		.scaleExtent([1, 9])
+		.scaleExtent([1, 600])
 		.translateExtent([
 			[0, 0],
 			[1 * width, 1 * height]
@@ -30,14 +31,14 @@ export function mapApi(nodeRef, ar, cb) {
 		.selectAll('path')
 		.data(feature(ar, ar.objects.provs).features)
 		.join('path')
-		.attr('class', 'county')
+		.attr('class', 'province')
 		.attr('id', function (d) {
-			return 'e' + d.properties.state_code;
+			return 'e' + d.id;
 		})
 		.on('click', clicked)
 		.attr('d', path);
 
-	provinces.append('title').text(d => d.properties.name);
+	provinces.append('title').text(d => d.properties.np);
 
 	g.append('path')
 		.attr('fill', 'none')
@@ -48,6 +49,8 @@ export function mapApi(nodeRef, ar, cb) {
 	svg.call(zoom);
 
 	function reset() {
+		active.classed('active', false);
+		active = d3.select(null);
 		provinces.transition().style('fill', null);
 		svg.transition()
 			.duration(750)
@@ -55,16 +58,40 @@ export function mapApi(nodeRef, ar, cb) {
 	}
 
 	function clicked(event, d) {
-		console.log(d.id.length);
+		if (active.node() === this) return reset();
+		active.classed('active', false);
+		active = d3.select(this).classed('active', true);
+		console.log(d.id);
 		if (d.id.length === 5) {
 			return;
-		} else {
-			g.selectAll('.featureMun').remove();
+		} else if (d.id === '01') {
+			console.log('capitalFederal')
+			g.selectAll('.capital').remove();
+			g.selectAll('.localidad').remove();
+			g.selectAll('#marcoCap').remove();
 			g.selectAll('#marcoMun').remove();
 			const [[x0, y0], [x1, y1]] = path.bounds(d);
 			event.stopPropagation();
 			svg.transition()
-				.duration(750)
+				.duration(850)
+				.call(
+					zoom.transform,
+					d3.zoomIdentity
+						.translate(width / 2, height / 2)
+						.scale(160, 0.9 / Math.max((x1 - x0) / width, (y1 - y0) / height))
+						.translate(-(x0 + x1) / 2, -(y0 + y1) / 2),
+					d3.pointer(event, svg.node())
+				);
+
+			const cap = getCapital(ar.objects.dptos.geometries)
+			dibujaCapital(cap)
+		} else {
+			g.selectAll('.localidad').remove();
+			g.selectAll('#marcoMun').remove();
+			const [[x0, y0], [x1, y1]] = path.bounds(d);
+			event.stopPropagation();
+			svg.transition()
+				.duration(850)
 				.call(
 					zoom.transform,
 					d3.zoomIdentity
@@ -77,11 +104,13 @@ export function mapApi(nodeRef, ar, cb) {
 			const mun = getMunicipios(d.id, ar.objects.dptos.geometries);
 			// console.log('mun', mun);
 			dibujaMunicipios(mun);
+
+
 		}
 	}
 
 	function zoomed(event, d) {
-		const {transform} = event;
+		const { transform } = event;
 		// console.log(transform);
 		g.attr('transform', transform);
 		g.attr('stroke-width', 1 / transform.k);
@@ -91,8 +120,8 @@ export function mapApi(nodeRef, ar, cb) {
 	function dibujaMunicipios(mun) {
 		// console.log('mun', mun);
 		// console.log('featMun', feature(ar, mun).features);
-		let municipios = g.selectAll('.featureMun').data(feature(ar, mun).features, function (d) {
-			return d.properties.claveMun;
+		let municipios = g.selectAll('.localidad').data(feature(ar, mun).features, function (d) {
+			return d.id;
 		});
 
 		municipios.exit().remove();
@@ -102,9 +131,9 @@ export function mapApi(nodeRef, ar, cb) {
 			.append('path')
 			.merge(municipios)
 			.attr('d', path)
-			.attr('class', 'featureMun')
+			.attr('class', 'localidad')
 			.attr('id', function (d) {
-				return 'm' + d.properties.claveMun;
+				return 'm' + d.id;
 			})
 			.on('click', function (e, d) {
 				console.log(d);
@@ -125,6 +154,82 @@ export function mapApi(nodeRef, ar, cb) {
 			.attr('stroke', '#F8F2F2')
 			.attr('stroke-linejoin', 'round');
 	}
+
+	function dibujaMunicipiosGBA(mun) {
+		// console.log('mun', mun);
+		// console.log('featMun', feature(ar, mun).features);
+		let municipiosGba = g.selectAll('.gba').data(feature(ar, mun).features, function (d) {
+			return d.id;
+		});
+
+		municipiosGba.exit().remove();
+
+		municipiosGba
+			.enter()
+			.append('path')
+			.merge(municipiosGba)
+			.attr('d', path)
+			.attr('class', 'gba')
+			.attr('id', function (d) {
+				return 'gba' + d.id;
+			})
+			.on('click', function (e, d) {
+				console.log(d);
+				console.log(d.properties.nd);
+				e.stopPropagation();
+			});
+
+		g.append('path')
+			.datum(
+				feature(ar, mun, function (a, b) {
+					return a !== b;
+				})
+			)
+			.attr('id', 'marcoMunGba')
+			.attr('class', 'mesh')
+			.attr('d', path)
+			.attr('fill', '#66a3ff')
+			.attr('stroke', '#F8F2F2')
+			.attr('stroke-linejoin', 'round');
+	}
+
+	function dibujaCapital(cap) {
+		console.log('cap', cap);
+		// console.log('featMun', feature(ar, mun).features);
+		let capital = g.selectAll('.capital').data(feature(ar, cap).features, function (d) {
+			return d.id;
+		});
+
+		capital.exit().remove();
+
+		capital
+			.enter()
+			.append('path')
+			.merge(capital)
+			.attr('d', path)
+			.attr('class', 'capital')
+			.attr('id', function (d) {
+				return 'm' + d.id;
+			})
+			.on('click', function (e, d) {
+				console.log(d);
+				console.log(d.properties.nd);
+				e.stopPropagation();
+			});
+
+		g.append('path')
+			.datum(
+				feature(ar, cap, function (a, b) {
+					return a !== b;
+				})
+			)
+			.attr('id', 'marcoCap')
+			.attr('class', 'mesh')
+			.attr('d', path)
+			.attr('fill', '#66a3ff')
+			.attr('stroke', '#F8F2F2')
+			.attr('stroke-linejoin', 'round');
+	}
 }
 
 function getMunicipios(idProvincia, municipios) {
@@ -139,3 +244,28 @@ function getMunicipios(idProvincia, municipios) {
 		geometries: filteredMun
 	};
 }
+
+function getCapital(municipios) {
+	// console.log('idProvincia', idProvincia);
+	// console.log('idProvincia', typeof idProvincia);
+	// console.log('municipios', municipios);
+	// console.log('municipio', municipios[0]);
+	let capitalFederal = municipios.filter(municipio => municipio.id.startsWith('01'));
+
+	return {
+		type: 'GeometryCollection',
+		geometries: capitalFederal
+	};
+}
+
+function getGba(municipios) {
+	const gba = ["02003", "02128", "02035", "02053", "02089", "02134", "02080", "02130", "02129", "02052", "02079", "02133", "02061", "02118", "02077", "02062", "02070", "02036", "02132", "02038", "02092", "02122", "02011", "02004", "02106", "02113", "02105"]
+
+	let filteredGba = municipios.filter(municipio => gba.includes(municipio.id));
+
+	return {
+		type: 'GeometryCollection',
+		geometries: filteredGba
+	};
+}
+
